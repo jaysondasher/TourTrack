cc9bc287b6057256aa09277e0de3a35e503b8779
jaysondasher <jaysondasher@gmail.com>
2023-06-13 23:08:20 -0400

    Added custom animated menu to the LocationView Cleaned up code

--- TourTrack/ViewModels/LocationsViewModel.swift
+++ TourTrack/ViewModels/LocationsViewModel.swift
@@ -14,12 +14,17 @@ class LocationsViewModel: ObservableObject {
     @Published var locations: [Location]
     // Current location on the map
     @Published var mapLocation: Location {
+        // On update to mapLocation, automatically update mapRegion
         didSet {
             updateMapRegion(location: mapLocation)
         }
     }
+    // Current region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -33,6 +38,19 @@ class LocationsViewModel: ObservableObject {
             mapRegion = MKCoordinateRegion(
                 center: location.coordinates,
                 span: mapSpan)
+        }
+    }
+    
+     func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeOut) {
+            mapLocation = location
+            showLocationsList = false
         }
     }
     
